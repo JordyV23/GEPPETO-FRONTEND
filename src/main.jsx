e0e1384dc8ebb65f2a3b2456154302a48704bf73
JordyV23 <jordyvargas0123@gmail.com>
2023-06-18 16:17:17 -0600

    Configuracion Inicial -Distribucion de carpetas -Instalacion de ciertas dependencias: * react-router-dom *  @reduxjs/toolkit * react-redux * dotenv * sweetalert2

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <p>HOLA MUNDO!</p>
+    <p>I'm GEPPETTO :D</p>
+  </React.StrictMode>,
+)
