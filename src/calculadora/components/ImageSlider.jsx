f2f2625ab602df8fd5a38f631ed6a761a6bc36cd
JordyV23 <jordyvargas0123@gmail.com>
2023-07-29 18:57:47 -0600

    Cambios realizados: -Componente de modal para mostrar instrucciones creado -Componente de slider creado para mostrar las intrucciones en el modal -Imagenes de instrucciones del modulo 1 agregadas -Boton de informacion para visualizar instrucciones en modulo1 agregado -Color para el boton agregando en el archivo de configuracion de tailwind -Archivo de barril de componentes actualizado

---
+++ src/calculadora/components/ImageSlider.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+
+export const ImageSlider = ({ imagenes }) => {
+
+  return (
+    <>
+      <div
+        id="default-carousel"
+        className="relative w-full"
+        data-carousel="static"
+      >
+        {/* <!-- Carousel wrapper --> */}
+        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+          {imagenes.map((imagen) => (
+            <div
+              key={imagen}
+              className="hidden duration-700 ease-in-out"
+              data-carousel-item
+            >
+              <img
+                src={imagen}
+                className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                alt="..."
+              />
+            </div>
+          ))}
+        </div>
+        {/* <!-- Indicadores del Slider --> */}
+        {/* <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+          {imagenes.map((i) => (
+            <button
+              key={i}
+              type="button"
+              className="w-3 h-3 bg-light-accent dark:bg-main-dark rounded-full"
+              aria-current={i===0 ? "true" : "false"}
+              aria-label={`Slide ${i+1}`}
+              data-carousel-slide-to={`${i}`}
+            ></button>
+          ))}
+        </div> */}
+        {/* <!--  Controles del Slider  --> */}
+        <button
+          type="button"
+          className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-prev
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-light-accent/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+        <button
+          type="button"
+          className=" absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-next
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-light-accent/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
+      </div>
+    </>
+  );
+};
