4d9534ead7398280c070509c23a66c76c791a471
Luis Fernando Salguero Guzman <lfsalguerog@gmail.com>
2023-08-09 11:29:25 -0600

    Se elimino la página sistemasNumericos(está comentada) Se agrego el modal para mostra las imagenes en el acordion

--- src/landing/components/YouTubeVideo.jsx
+++ src/landing/components/YouTubeVideo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const YouTubeVideo=({videoId}) =>{
   return (
-    <div className="p-6 w-11/12 h-64 sm:h-80 lg:h-[28rem] sm:w-9/12 mx-auto" >
+    <div className="p-6 w-11/12 h-64 sm:h-80 lg:h-[28rem] lg:w-6/12 sm:w-9/12 mx-auto" >
       <iframe className="mx-auto w-full h-full"
         src={`https://www.youtube.com/embed/${videoId}`}
         title="YouTube Video"
