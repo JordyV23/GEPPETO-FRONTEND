53070b9bee98020b558663f529b414bb99124527
JordyV23 <jordyvargas0123@gmail.com>
2023-08-13 09:32:18 -0600

    Cambios realizados: -Limpieza en paginas -Eliminado de paquete de mathquill ya que da problemas al subir a netlify

--- src/calculadora/pages/ProblemsPage.jsx
+++ src/calculadora/pages/ProblemsPage.jsx
@@ -1,24 +1,12 @@
-import axios from "axios";
-import { useForm } from "react-hook-form";
-import { Button, Textarea } from "flowbite-react";
 import {
-  ImageModal,
   ChatComponent,
   InputMessageComponent,
 } from "../components";
-import { Cog8ToothIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "animate.css";
 import "hover.css";
-import React, { useContext } from "react";
-import { addStyles, EditableMathField } from "react-mathquill";
-
-addStyles();
 
 export const ProblemsPage = () => {
-
   return (
     <>
       <div id="ProblemsPage" className="p-4 border-2 items-center w-full h-full justify-center border-white border-dashed rounded-lg dark:border-white mt-20">
